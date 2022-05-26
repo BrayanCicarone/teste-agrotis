8625320ee68238e7d69a0feac86b8892008e6c89
brayan.cicarone@mouts.info <Bac1025548>
2022-05-26 11:15:41 -0300

    Start project

---
+++ src/main/java/com/agrotis/agrotiscrud/AgrotisCrudApplication.java
@@ -0,0 +1,13 @@
+package com.agrotis.agrotiscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrotisCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgrotisCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agrotis/agrotiscrud/AgrotisCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrotis.agrotiscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrotisCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
